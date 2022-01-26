eaab979170a5b7504f36f51a190990ef2e570e0c
Genia Boeing <genia@gboeing.com>
2022-01-26 11:25:55 +0100

    Add problem 3

---
+++ ReverseComplement.py
@@ -0,0 +1,18 @@
+input_file = 'Data/rosalind_revc.txt'
+with open(input_file) as f:
+    data = f.read()
+data = data.strip()
+
+
+complement = {'A': 'T', 'T': 'A', 'G': 'C', 'C': 'G'}
+
+def getReverseComplement(data):
+    reverse = [0] * len(data)
+    
+    for i in range(0, len(data)):
+        reverse[-i - 1] = complement[data[i]]
+
+    reverseStrand = ''
+    return reverseStrand.join(reverse)
+
+print(getReverseComplement(data))
